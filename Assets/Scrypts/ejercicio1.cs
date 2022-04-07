45fe5e452796a51b862578ccfb31b562bbec4558
LaPietraFranco <101827023+LaPietraFranco@users.noreply.github.com>
2022-04-07 15:52:16 -0300

    ejercicio1

---
+++ Assets/Scrypts/ejercicio1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio1 : MonoBehaviour
+{
+    public int n1;
+    public int n2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (n1 < n2)
+        {
+            Debug.Log("los numeros son distintos");
+        }
+        else if (n1 > n2)
+        {
+            Debug.Log("los numeros son distintos");
+        }
+        else
+        {
+            Debug.Log("los numeros son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
